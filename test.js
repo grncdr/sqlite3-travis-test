a6eeaf7cbd6fe1883d9dc835a0e26ca720fb6d9a
Stephen Sugden <glurgle@gmail.com>
2013-05-11 19:20:47 -0700

    ok, this should work

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-var db = new require('sqlite3').Database('/tmp/test.db')
+var Database = require('sqlite3').Database
+var db = new Database('/tmp/test.db')
 db.serialize();
 db.each('create table blah (yeah int)')
 db.each('insert into blah (yeah) values (1)')
