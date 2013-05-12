13b966fe74e1d9d447b149f6abb34bcaf1fbe55d
Stephen Sugden <glurgle@gmail.com>
2013-05-11 19:18:12 -0700

    trigger travis build

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
 var db = new require('sqlite3').Database('/tmp/test.db')
 db.serialize();
 db.each('create table blah (yeah int)')
+db.each('insert into blah (yeah) values (1)')
+db.each('select * from blah')
