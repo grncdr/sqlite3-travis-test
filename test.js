5f0fca04767bc3ac01fda9b0d9bbc9858daac5a1
Stephen Sugden <glurgle@gmail.com>
2013-05-11 19:23:10 -0700

    More minimal

--- test.js
+++ test.js
@@ -1,6 +1,3 @@
 var Database = require('sqlite3').Database
 var db = new Database('/tmp/test.db')
 db.serialize();
-db.each('create table blah (yeah int)')
-db.each('insert into blah (yeah) values (1)')
-db.each('select * from blah')
