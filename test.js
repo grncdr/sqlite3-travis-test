020fb64421d68953f31100e8d2170f32f585f21a
Stephen Sugden <glurgle@gmail.com>
2013-05-11 19:27:53 -0700

    closer...

--- test.js
+++ test.js
@@ -2,3 +2,4 @@ var Database = require('sqlite3').Database
 var db = new Database('/tmp/test.db')
 db.serialize();
 db.each('create table blah (yeah int)')
+db.each('insert into blah (yeah) values (1)')
